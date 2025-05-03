ddb92eafe358d185e8c0c8fa3403ae24d55b5217
Abhishek kumar <ak4279673@gmail.com>
2025-05-04 00:33:01 +0530

    Deployment Configuration

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 
 from sklearn.metrics import r2_score
 from src.exception import customException
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -19,13 +20,20 @@ def save_object(file_path, obj):
     except Exception as e:
         raise customException(e, sys)
 
-def evaluate_model(X_train,y_train , X_test,y_test,models):
+def evaluate_model(X_train,y_train , X_test,y_test,models,param):
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
             #train model
-            model.fit(X_train , y_train)
+            # model.fit(X_train , y_train)
 
             y_train_pred = model.predict(X_train)
 
@@ -38,7 +46,14 @@ def evaluate_model(X_train,y_train , X_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
-    
+
+    except Exception as e:
+        raise customException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb')as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise customException(e,sys)
         
